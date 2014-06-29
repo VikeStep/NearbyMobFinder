e9511ca028511ca2f7990ecdbab867463b9af3a8
Nabijaczleweli <nabijaczleweli@gmail.com>
2014-06-30 00:40:04 +0200

    Make "Future Planned Features" #1 done.
    Ability to find nearby mobs at any time of day (disabled by default in config)

---
+++ src/main/java/com/vikestep/nearbymobfinder/reference/Settings.java
@@ -0,0 +1,9 @@
+package com.vikestep.nearbymobfinder.reference;
+
+import net.minecraftforge.common.config.Configuration;
+
+public class Settings {
+    public static Configuration config;
+
+    public static boolean showMobsAtAnyTimeOfDay;
+}
