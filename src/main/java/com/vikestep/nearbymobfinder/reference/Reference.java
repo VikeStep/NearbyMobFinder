416b72bf813c35b2b2a0b8d93e932a19086d7d4d
VikeStep <vikestepftb@gmail.com>
2014-11-16 21:40:29 +1000

    Bump Version Number

--- src/main/java/com/vikestep/nearbymobfinder/reference/Reference.java
+++ src/main/java/com/vikestep/nearbymobfinder/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "NearbyMobFinder";
     public static final String MOD_NAME = "Nearby Mob Finder";
-    public static final String VERSION = "1.7.10-1.1.0";
+    public static final String VERSION = "1.7.10-1.1.1";
     public static final String CLIENT_PROXY_PATH = "com.vikestep.nearbymobfinder.proxy.ClientProxy";
     public static final String SERVER_PROXY_PATH = "com.vikestep.nearbymobfinder.proxy.ServerProxy";
 }
